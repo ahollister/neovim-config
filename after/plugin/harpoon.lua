ce4faa5623b7001d01dd6b31bdfd3ebbfc054765
ahollister <aaahollister@gmail.com>
2023-02-07 12:32:33 +0000

    Add harpoon terminal and leader+c for toggling cmdheight

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -3,6 +3,7 @@
 
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
+local term = require("harpoon.term")
 
 -- Keybindings
 vim.keymap.set("n", "<leader>ha", mark.add_file)
@@ -10,3 +11,6 @@ vim.keymap.set("n", "<leader>hs", ui.toggle_quick_menu)
 vim.keymap.set("n", "<leader><Up>", ui.toggle_quick_menu)
 vim.keymap.set("n", "<leader><Right>", ui.nav_next)
 vim.keymap.set("n", "<leader><Left>", ui.nav_prev)
+vim.keymap.set("n", "<leader><Down>", function()
+    term.gotoTerminal(1)
+end)
--- lua/main/remap.lua
+++ lua/main/remap.lua
@@ -13,3 +13,13 @@ vim.keymap.set("n", "<leader>n", ":set invnumber invrelativenumber<CR>")
 -- Toggle whitespace
 vim.keymap.set("n", "<leader>w", ":set list!<CR>")
 
+-- Toggle cmdheight
+vim.keymap.set("n", "<leader>c", function()
+    local cmdheight = vim.api.nvim_get_option("cmdheight")
+    if cmdheight == 1 then
+        vim.api.nvim_set_option("cmdheight", 0)
+    else
+        vim.api.nvim_set_option("cmdheight", 1)
+    end
+end)
+
