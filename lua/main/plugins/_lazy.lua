eebc980558988afbc72951bd95b93cfde437d282
ahollister <aaahollister@gmail.com>
2023-10-27 11:31:31 +0100

    Add colorizer

--- lua/main/plugins/_lazy.lua
+++ lua/main/plugins/_lazy.lua
@@ -194,6 +194,23 @@ require("lazy").setup({
 	-- Vim Cool - Handle search highlighting better
 	{ "romainl/vim-cool" },
 
+	-- Colorizer
+	{
+		"NvChad/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({
+				filetypes = { "*" },
+				user_default_options = {
+					css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+					css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+					tailwind = true, -- Enable tailwind colors
+					-- parsers can contain values used in |user_default_options|
+					sass = { enable = true, parsers = { "css" } }, -- Enable sass colors
+				},
+			})
+		end,
+	},
+
 	-- Colorscheme - tokyonight
 	{
 		"folke/tokyonight.nvim",
