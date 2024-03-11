bcf548d2de9b7e0e478ced5d6249afbb998541a8
ahollister <aaahollister@gmail.com>
2024-03-11 02:19:40 +0000

    nota config

--- lua/main/plugins/nota.lua
+++ lua/main/plugins/nota.lua
@@ -3,8 +3,9 @@ local M = {}
 M.config = function()
 	require("nota").setup({
 		global_path = "/Users/adam/notes.md",
+		scratch_path = "/Users/adam/scratch.md",
 		local_path = "notes.md",
-		vertical_split = false,
+		vertical_split = true,
 	})
 end
 
